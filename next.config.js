467c182807b4274720d06a9f9074d814cb93d697
Andon Mitev <mitevandon94@gmail.com>
2023-07-06 16:08:02 +0300

    update deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({
+        bufferutil: 'bufferutil',
+        lokijs: 'lokijs',
+        encoding: 'encoding',
+        'utf-8-validate': 'utf-8-validate',
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
